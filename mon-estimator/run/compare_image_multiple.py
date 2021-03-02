4c6bb49da358ccdc358e7b1340be9ada6bc6b5f0
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-03-02 16:16:37 +0100

    update use of specific methods

--- mon-estimator/run/compare_image_multiple.py
+++ mon-estimator/run/compare_image_multiple.py
@@ -37,45 +37,46 @@ def main():
 
     counter = 0
     
-    for method in ["border", "crop"]:
+    for i, est in enumerate(estimators):
+
+        # get current expected method for estimator
+        method = json_data['methods'][i]
 
         default_estimator_path = os.path.join(p_folder, method, reference)
 
         # expected images path have same name
         images = os.listdir(default_estimator_path)
+        
+        # prepare output filename
+        counter_str = str(counter)
 
-        for est in estimators:
+        while len(counter_str) < 3:
+            counter_str = "0" + counter_str
+            
+        output_filename = os.path.join(p_output, f"{counter_str}_{method}_{reference}_{est}_{metric}.csv")
+        output_file = open(output_filename, 'w')
 
-            # prepare output filename
-            counter_str = str(counter)
+        for img in sorted(images):
 
-            while len(counter_str) < 3:
-                counter_str = "0" + counter_str
+            est1_path_image = os.path.join(p_folder, method, reference, img)
+            est2_path_image = os.path.join(p_folder, method, est, img)
+
+            img_rgb_1 = np.array(Image.open(est1_path_image))
+            img_rgb_2 = np.array(Image.open(est2_path_image))
+
+            scene_name = img.replace('.png', '')
+
+            if metric == 'ssim':
+                sentence = "{0};{1};{2};{3}\n".format(scene_name, img, estimators, ssim(img_rgb_1, img_rgb_2, multichannel=True))
+                output_file.write(sentence)
+
+            if metric == 'rmse':
+                sentence = "{0};{1};{2};{3}\n".format(scene_name, img, estimators, rmse(img_rgb_1, img_rgb_2))
+                output_file.write(sentence)
+
+        counter += 1
                 
-            output_filename = os.path.join(p_output, f"{counter_str}_{method}_{reference}_{est}_{metric}.csv")
-            output_file = open(output_filename, 'w')
-
-            for img in sorted(images):
-
-                est1_path_image = os.path.join(p_folder, method, reference, img)
-                est2_path_image = os.path.join(p_folder, method, est, img)
-
-                img_rgb_1 = np.array(Image.open(est1_path_image))
-                img_rgb_2 = np.array(Image.open(est2_path_image))
-
-                scene_name = img.replace('.png', '')
-
-                if metric == 'ssim':
-                    sentence = "{0};{1};{2};{3}\n".format(scene_name, img, estimators, ssim(img_rgb_1, img_rgb_2, multichannel=True))
-                    output_file.write(sentence)
-
-                if metric == 'rmse':
-                    sentence = "{0};{1};{2};{3}\n".format(scene_name, img, estimators, rmse(img_rgb_1, img_rgb_2))
-                    output_file.write(sentence)
-
-            counter += 1
-                    
-        output_file.close()
+    output_file.close()
             
 
 
