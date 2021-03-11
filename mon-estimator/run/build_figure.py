0e06efb8f536f9362089b7797a33bf5ff1448b25
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-03-11 16:56:56 +0100

    unique id for folder and compute all comparisons

--- mon-estimator/run/build_figure.py
+++ mon-estimator/run/build_figure.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 
+import uuid
 
 # Expected JSON format
 # folder need to have same number of files of each elements in json
@@ -51,7 +52,7 @@ from skimage.metrics import structural_similarity as ssim
 # 3 => estimator
 # 4 => image_path
 # 5 => score
-figure_str = "\\begin{{subfigure}}[t]{{{0}\\textwidth}}\n\t\centering\n\t\includegraphics[width=\\textwidth]{{{1}/{2}/{3}/{4}}}\n\t\\footnotesize{{{5}: {6:.2f}}}\n\end{{subfigure}}\n"
+figure_str = "\\begin{{subfigure}}[t]{{{0}\\textwidth}}\n\t\centering\n\t\includegraphics[width=\\textwidth]{{{1}}}\n\t\\footnotesize{{{2}: {3:.2f}}}\n\end{{subfigure}}\n"
 
 end_str = "\\begin{{subfigure}}[t]{{{0}\\textwidth}}\n\t\centering\n\t{1}\n\end{{subfigure}}\n"
 
@@ -93,6 +94,11 @@ def main():
             
             comparisons.append(current_comparisons)
 
+    unique_output_folder = os.path.join(json_data['output'], str(uuid.uuid4()))
+
+    if not os.path.exists(unique_output_folder):
+        os.makedirs(unique_output_folder)
+
     for id_scene, scene in enumerate(json_data["scenes"]):
         
         output_f.write("% {0}\n".format(scene))
@@ -109,7 +115,16 @@ def main():
             # 3 => estimator
             # 4 => image_path
             # 5 => score
-            output_f.write(figure_str.format(figsize, os.path.join(json_data['output'], json_data['nsamples'], 'processing'), method, est, img_name, json_data["metric"].upper(), score))
+            current_image_name = os.path.join(json_data['output'], json_data['nsamples'], 'processing', method, est, img_name)
+
+            # HERE for each line create unique identifier
+            output_unique_image = str(uuid.uuid4()) + '.png'
+            output_image_path = os.path.join(unique_output_folder, output_unique_image)
+
+            os.system(f'cp {current_image_name} {output_image_path}')
+
+            output_f.write(figure_str.format(figsize, output_image_path, json_data["metric"].upper(), score))
+
 
         if id_scene < len(json_data["scenes"]) - 1:
             output_f.write("\n\\vspace{2mm}\n~\n\n")
---
+++ mon-estimator/run/compute_comparisons.py
@@ -0,0 +1,114 @@
+import os, sys
+import argparse
+
+import numpy as np
+from PIL import Image
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error as rmse
+
+def write_progress(progress):
+    '''
+    Display progress information as progress bar
+    '''
+    barWidth = 180
+
+    output_str = "["
+    pos = barWidth * progress
+    for i in range(barWidth):
+        if i < pos:
+           output_str = output_str + "="
+        elif i == pos:
+           output_str = output_str + ">"
+        else:
+            output_str = output_str + " "
+
+    output_str = output_str + "] " + str(int(progress * 100.0)) + " %\r"
+    print(output_str)
+    sys.stdout.write("\033[F")
+
+def compare_image(metric, ref, image):
+
+    error = None
+    if metric == 'ssim':
+        error = ssim(ref, image, multichannel=True)
+
+    if metric == 'rmse':
+        error = rmse(ref, image)
+
+    return error
+
+def main():
+    
+    parser = argparse.ArgumentParser(description="Compare all estimators to reference image for few scenes")
+
+    parser.add_argument('--estimators', type=str, help="folder with all estimators", required=True)
+    parser.add_argument('--reference', type=str, help="reference folder with scenes", required=True)
+    parser.add_argument('--metric', type=str, help="metric choice to use", choices=['ssim', 'rmse'], required=True)
+    parser.add_argument('--output', type=str, help="output data file", required=True)
+
+    args = parser.parse_args()
+
+    estimators = args.estimators
+    reference = args.reference
+    metric = args.metric
+    output = args.output
+
+    output_path, _ = os.path.split(output)
+
+    if len(output_path) > 0 and not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    f = open(output, 'w')
+
+    print('Load of reference images')
+    reference_images = {}
+    for scene in sorted(os.listdir(reference)):
+        scene_path = os.path.join(reference, scene)
+        image_name = os.listdir(scene_path)[0]
+
+        image_path = os.path.join(scene_path, image_name)
+        reference_images[scene] = np.array(Image.open(image_path))
+
+    print('For each estimator start to compare each scene')
+    for est in sorted(os.listdir(estimators)):
+        
+        est_path = os.path.join(estimators, est)
+        for scene in sorted(os.listdir(est_path)):
+
+
+            print(f'Comparisons of {est} to reference on {scene} scene')
+            data_line = f'{est};{scene}'
+
+            scene_path = os.path.join(est_path, scene)
+
+            # get all image names
+            scene_images = sorted(os.listdir(scene_path))
+            nimages = len(scene_images)
+
+            counter = 0
+
+            if scene not in reference_images:
+                print(f'{scene} data not available for reference')
+
+            for img in scene_images:
+
+                # open image data
+                img_path = os.path.join(scene_path, img)
+                img_data = np.array(Image.open(img_path))
+
+                # compare image
+                current_error = compare_image(metric, reference_images[scene], img_data)
+
+                # save data into line
+                data_line += f';{current_error}'
+
+                # display information
+                counter += 1
+                write_progress((counter + 1) / nimages)
+
+            f.write(data_line)
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
